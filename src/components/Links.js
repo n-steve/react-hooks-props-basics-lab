f16222b2ad1a7325a4d4b17a99fe8cd1259acc17
steve <steve.nguyen2013@gmail.com>
2022-05-28 18:54:51 -0700

    edit

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,8 +5,8 @@ function Links(props) {
   return (
     <div>
       <h3>Links</h3>
-      <a href="https://github.com/liza">{props.github}</a>
-      <a href="https://www.linkedin.com/in/liza/">{props.linkedin}</a>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
     </div>
   );
 }
